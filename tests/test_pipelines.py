e94bb7fc3be2eb6aaf762d2f597a154e64053de0
StefanBRas <gitcommits@bruhn.io>
2022-11-20 01:56:46 +0100

    add steps parameter template test

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,5 +1,5 @@
 from ado_pipeline_helper import Client, Run
-from ado_pipeline_helper.yaml_loader import unordered_yaml as yaml
+from ado_pipeline_helper.yaml_loader import yaml
 
 def test_preview(client: Client):
     preview = client.preview()
@@ -11,9 +11,5 @@ def test_validate(client: Client):
     validated = client.validate()
     assert isinstance(preview, Run)
     assert isinstance(validated, Run), client.load_yaml()
-    assert yaml.load(validated.final_yaml)['stages'][0]['jobs']== yaml.load(preview.final_yaml)['stages'][0]['jobs']
-    assert yaml.load(validated.final_yaml)['stages'][0]== yaml.load(preview.final_yaml)['stages'][0]
-    assert yaml.load(validated.final_yaml)['stages']== yaml.load(preview.final_yaml)['stages']
     assert yaml.load(validated.final_yaml) == yaml.load(preview.final_yaml)
-    assert False
 
