32f13af5eecfa0e6238b5b2c26cda3e922bf7373
StefanBRas <gitcommits@bruhn.io>
2022-11-20 14:03:55 +0100

    add test for job parameters

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,5 +1,4 @@
 from ado_pipeline_helper import Client, Run
-from ado_pipeline_helper.yaml_loader import yaml
 
 def test_preview(client: Client):
     preview = client.preview()
@@ -10,10 +9,7 @@ def test_validate(client: Client):
     preview = client.preview()
     validated = client.validate()
 
-    assert isinstance(preview, Run)
-    assert isinstance(validated, Run), client.load_yaml()
-    print(client.load_yaml())
+    assert isinstance(preview, Run), preview
+    assert isinstance(validated, Run), (validated, client.load_yaml())
     assert validated.final_yaml == preview.final_yaml
-    assert yaml.load(validated.final_yaml) == yaml.load(preview.final_yaml)
-    assert False
 
